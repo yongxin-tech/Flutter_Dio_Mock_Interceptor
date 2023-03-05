767f8c060f75d999032f123040eb7e792ca100c4
MING-7L1U166\user <page700414@gmail.com>
2023-03-05 20:48:07 +0800

    add example for pub.dev

---
+++ example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:dio_mock_interceptor/dio_mock_interceptor.dart';
+
+void main() async {
+  Dio dio = Dio(BaseOptions());
+
+  dio.interceptors.add(MockInterceptor());
+
+  Response response =
+      await dio.post("/api/login"); // the same path as common.json
+  String json = response.data;
+  if (json.isEmpty) {
+    throw Exception('response is empty');
+  }
+
+  Map<String, dynamic> data = jsonDecode(json);
+  bool isSuccess = data['success'] as bool;
+  print(isSuccess); // true
+  Map<String, dynamic> result = data['result'];
+  print(result['test']); //test
+}
